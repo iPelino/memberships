cc3208a0461402bf06f30afb18d7ce63fa2c52d1
pelino <pelmut2000@gmail.com>
2018-12-05 16:37:55 +0200

    Added Entity Classes

---
+++ Memberships/Entities/Section.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Entities
+{
+/*
+ * Creating Section table: code first approach
+ */
+    [Table("Section")]
+    public class Section
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string title { get; set; }
+    }
+}
